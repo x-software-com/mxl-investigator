a959db2f72beab0760543519910640a9e101083d
acpiccolo <60399193+acpiccolo@users.noreply.github.com>
2024-03-21 12:58:06 +0100

    fix: initialization

--- src/misc.rs
+++ src/misc.rs
@@ -6,14 +6,15 @@ pub(crate) const SUPPORT_EMAIL: &str = "support@x-software.com";
 
 static PROJECT_DATA_DIR: OnceCell<PathBuf> = OnceCell::new();
 
-pub fn init(project_data_dir: PathBuf) -> anyhow::Result<()> {
+pub fn init(project_data_dir: PathBuf) {
     PROJECT_DATA_DIR.set(project_data_dir).expect("Already initialized");
     crate::localization::init();
-    #[cfg(any(feature = "create_report_dialog", feature = "problem_report_dialog"))]
-    {
-        mxl_relm4_components::init()?;
-        relm4_icons::initialize_icons();
-    }
+}
+
+#[cfg(any(feature = "create_report_dialog", feature = "problem_report_dialog"))]
+pub fn init_gui() -> anyhow::Result<()> {
+    mxl_relm4_components::init()?;
+    relm4_icons::initialize_icons();
     Ok(())
 }
 
@@ -22,11 +23,11 @@ pub(crate) fn get_data_dir() -> &'static PathBuf {
 }
 
 #[cfg(feature = "with_test")]
-pub fn init_test() -> anyhow::Result<()> {
+pub fn init_test() {
     use once_cell::sync::Lazy;
     use tempfile::TempDir;
 
     static TMP_DIR: Lazy<TempDir> = Lazy::new(|| TempDir::new().expect("Failed create tmp directory"));
 
-    init(TMP_DIR.path().to_path_buf())
+    init(TMP_DIR.path().to_path_buf());
 }
