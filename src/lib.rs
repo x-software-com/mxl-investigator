38b6102fdb436fcde1ae844c53e85d1cfea18565
acpiccolo <60399193+acpiccolo@users.noreply.github.com>
2024-03-21 13:03:08 +0100

    fix: inisialization

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,9 @@ pub mod create_report_dialog;
 #[cfg(feature = "problem_report_dialog")]
 pub mod problem_report_dialog;
 
+#[cfg(any(feature = "create_report_dialog", feature = "problem_report_dialog"))]
+pub use misc::init_gui;
+
 pub use misc::init;
 
 #[cfg(feature = "with_test")]
